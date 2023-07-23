41ded65e4fbd67c21255a8406ff771635399262d
yannik1015 <34042618+yannik1015@users.noreply.github.com>
2023-07-23 20:11:20 +0200

    Added Options Flow

--- custom_components/goecharger/config_flow.py
+++ custom_components/goecharger/config_flow.py
@@ -2,9 +2,13 @@ import logging
 from datetime import timedelta
 import voluptuous as vol
 
-from homeassistant import config_entries
+from typing import Any
+
+# from homeassistant import config_entries
+from homeassistant.config_entries import ConfigEntry, OptionsFlow, ConfigFlow
 from homeassistant.core import callback
 from homeassistant.helpers import selector
+from homeassistant.data_entry_flow import FlowResult
 
 from homeassistant.const import CONF_HOST, CONF_SCAN_INTERVAL
 from .const import DOMAIN, CONF_NAME, CONF_CORRECTION_FACTOR, CONF_API_LEVEL
@@ -15,13 +19,13 @@ DEFAULT_UPDATE_INTERVAL = timedelta(seconds=20)
 MIN_UPDATE_INTERVAL = timedelta(seconds=10)
 
 
-class ConfigFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
     """Config flow for go-eCharger setup."""
     VERSION = 1
 
     @staticmethod
     @callback
-    async def async_get_options_flow(config_entry):
+    async def async_get_options_flow(config_entry: ConfigEntry) -> OptionsFlow:
         return OptionsFlowHandler(config_entry)
 
     async def async_step_user(self, info):
@@ -45,4 +49,34 @@ class ConfigFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                     ),
                 }
             ),
+        )
+
+class OptionsFlowHandler(OptionsFlow):
+    """Options flow for the go-eCharger"""
+
+    def __init__(self, config_entry: ConfigEntry) -> None:
+        """"Initialize options flow"""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input: dict[str, Any] | None = None) -> FlowResult:
+        """Manage options for the goe-Charger component"""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_HOST): str,
+                    vol.Optional(
+                        CONF_SCAN_INTERVAL, default=20
+                    ): int,
+                    vol.Required(
+                        CONF_CORRECTION_FACTOR, default="1.0"
+                    ): str,
+                    vol.Required(CONF_API_LEVEL, default="1"): selector.selector(
+                        {"select": {"mode": "dropdown", "options": ["1", "2"]}}
+                    ),
+                }
+            )
         )
